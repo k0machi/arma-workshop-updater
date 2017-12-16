105a665fbfdef3fe986d2f07306d2a09e4897f97
Komachi <xenobyterus@gmail.com>
2017-12-16 20:34:44 +0300

    Add project files.

---
+++ a3-workshop-downloader-csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a3_workshop_downloader_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
